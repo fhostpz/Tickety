e67b41b2fd2b15c28c1c58075e463a0fd0325b5c
jameszaii <james0523njk@gmail.com>
2018-02-15 05:30:33 +0800

    Add files via upload

--- eventpage.php
+++ eventpage.php
@@ -17,50 +17,12 @@
   //select db
   $db_selected = mysqli_select_db($link, "event");
 
- $nameErr = $locErr = $startErr = $endErr = "";
-  $name = $loc = $start = $end = "";
 
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
-      $nameErr = "Name is required";
-    } else {
-      $name = test_input($_POST["event"]);
-    }
-    
-    if (empty($_POST["email"])) {
-      $locErr = "Email is required";
-    } else {
-      $loc = test_input($_POST["location"]);
-    }
-      
-    if (empty($_POST["website"])) {
-      $startErr = "";
-    } else {
-      $start = test_input($_POST["start_datetime"]);
-    }
+  $sql = "INSERT INTO eventpage(e_name, e_loc, e_region,startDatetime,endDatetime,  e_dscp) VALUES ( '$_POST[event]' , '$_POST[location]' ,  '$_POST[region]' , '$_POST[start_datetime]', '$_POST[end_datetime]',   '$_POST[description]' )";
 
-    if (empty($_POST["comment"])) {
-      $endErr = "";
-    } else {
-      $end = test_input($_POST["start_datetime"]);
-    }
-
-  }
-
-  else{
-    $sql = "INSERT INTO eventpage(e_name, e_loc, e_country,startDatetime,endDatetime,  e_dscp) VALUES ( '$_POST[event]' , '$_POST[location]' ,  '$_POST[country]' , '$_POST[start_datetime]', '$_POST[end_datetime]',   '$_POST[description]' )";
-
-    mysqli_query($link, $sql); 
-  }
+  mysqli_query($link, $sql); 
 
   header("Location: http://localhost/event/eventpage.html");
-
-    function test_input($data) {
-      $data = trim($data);
-      $data = stripslashes($data);
-      $data = htmlspecialchars($data);
-      return $data;
-    }
 
     
 ?>
